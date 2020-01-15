2e6ec44f6ce4b927cd4b723b09964bffa0e8737a
Francis De Brabandere <francisdb@gmail.com>
2020-01-15 11:23:26 +0100

    fix license header

--- src/main/java/eu/somatik/maven/serviceloader/ServiceloaderMojo.java
+++ src/main/java/eu/somatik/maven/serviceloader/ServiceloaderMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Francis De Brabandere <info@somatik.eu>
+ * Copyright (C) 2020 Francis De Brabandere <info@somatik.eu>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
